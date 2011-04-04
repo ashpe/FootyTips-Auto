a049feb4348e5c2ae09631b8bf47697e3730172f
Ashley Pope <ashleyp@hydraflux.mel.strategicdata.com.au>
2011-04-04 17:06:02 +1000

    Fully working mechanize to do footytips.com.au afl tips. Pass in an array of team names in the correct format and the script will do the rest :)

--- mechanize.pl
+++ mechanize.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 
 
+# Do football tips on website footytips.com.au
+
 use Modern::Perl;
 use WWW::Mechanize;
 use Storable;
@@ -22,10 +24,20 @@ $url = 'http://www.footytips.com.au/tipping/afl/';
 $m->get($url);
 
 my @tmp_content = split "\n", $m->content;
-my @arr = ('collingwood', 'western bulldogs', 'adelaide', 'hawthorn', 'west coast eagles', 'melbourne', 'geelong', 'essendon');
 
+foreach (@tmp_content) {
+    if (/\?ff=Tips/i) {
+        /href=\"(.+)\" title=\"Edit tips\" class=\"ft_icon16EditTips\"/i;
+        $m->get("http://www.footytips.com.au" . $1);
+        last if $m->success;
+    }
+}
+
+@tmp_content = split "\n", $m->content;
+my @arr = ('collingwood', 'western bulldogs', 'fremantle', 'hawthorn', 'west coast eagles', 'melbourne', 'geelong', 'essendon');
 my $teams = join "|", @arr; 
 my @id_for_tips;
+my $tmp;
 
 #$mech->set_fields('one_of_two_options'=>'option_b')
 #<input id="Margin" class="ftform" type="text" size="3" value="22" name="Margin">
@@ -35,19 +47,19 @@ my @id_for_tips;
 #$m->set_fields(MATCH21 => 27);
 foreach (@tmp_content) {
     if (/for=\"(.*)\">($teams)<\/label>/i) {
-        push @id_for_tips, $1;
-    } elsif (/id=\"$id_for_tips[0]\"/) {
-        /name=\"(.*)\"/i;
-        print "First: $1\n";
-        /value=\"(.*)\"/i;
-        print "Second: $1\n";
-        #print "$_\n\n";
+        $tmp = $1;
+        foreach my $c (@tmp_content) {
+            if ($c =~ /id=\"$tmp\"/) {
+                $c =~ /name=\"(.+)\" value=\"(.+)\"/i;
+                push @id_for_tips, {name => $1, value => $2};
+            }
+        }
     }
 }
 
+
+foreach (@id_for_tips) {
+    $m->set_fields($_->{name} => $_->{value});
+}
+
 $m->click_button(name => 'saveTips');
-
-print "\n";
-print Dumper(@id_for_tips);
-
-
