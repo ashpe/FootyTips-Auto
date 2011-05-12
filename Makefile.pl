338b5c099d0568d19c358b82b002d30419bc2055
ashpe <ashpe@1tip.ws>
2011-05-12 11:11:55 +0400

    Created new makefile.pl, updated css for website and have webservice/database/website all running on the same VPS now.

---
+++ Makefile.pl
@@ -0,0 +1,21 @@
+   38      curl -L http://cpanmin.us | perl - --sudo App::cpanminus
+   39  cpanm Dancer
+   40* cpanm --sudo 
+   43  cpanm WWW::Mechanize
+   44  vim /home/ashpe/.cpanm/build.log 
+   45  cpanm --force WWW::Mechanize
+   46  cpanm Moose
+   59  cpanm Plack
+   61  cpanm Starman
+   62  cpanm --force Starman
+   64  cpanm YAML
+   66  cpanm Template
+   68  cpanm Modern::Perl
+   70  cpanm Config::ZOMG
+   72  cpanm RPC::XML
+   74  cpanm Time::Format
+   76  cpanm YAML::XS
+   78  cpanm --force DBD::mysql
+   81  cpanm --prompt DBD::mysql
+   83  history | grep cpanm
+   84  history | grep cpanm >> Makefile.pl
